9861d89b37806e28e78aa2ec2995b5453e52c869
lvxnull2 <184518908+lvxnull2@users.noreply.github.com>
2024-10-16 18:04:41 +0200

    Display started pods/deployments

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ install -Dm644 container@.service -t "${XDG_DATA_HOME:-$HOME/.local/share}/syste
 
 for f in */deployment.yaml; do
   d="$(dirname "$f")"
+  echo "Starting $d"
   systemctl --user enable --now "$(systemd-escape --template container@.service "$d")"
 done
